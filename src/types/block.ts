a86329e5914c9be5d2c9d7b39b8293b888150913
零泠丶 <632177447@qq.com>
2026-06-03 15:52:47 +0800

    refactor(types): 拆分全局类型文件 index.ts 并作为统一导出入口

---
+++ src/types/block.ts
@@ -0,0 +1,20 @@
+export const BlockType = {
+  AIR: 0,
+  GRASS: 1,
+  DIRT: 2,
+  STONE: 3,
+  WOOD: 4,
+  LEAF: 5,
+  BRICK: 6,
+  GLASS: 7,
+  WATER: 8,
+  SAND: 9,
+  COAL: 10,
+  IRON: 11,
+  DIAMOND: 12,
+  CHEST: 13,
+  LEVER: 14,
+} as const;
+export type BlockType = typeof BlockType[keyof typeof BlockType];
+
+export const BLOCK_TYPES = BlockType;
